cc70cd64f07e878aba011e3971064e6a9e60e043
aeksei <aeksei94@gmail.com>
2020-10-20 11:36:59 +0300

    for / if product

--- shop/views.py
+++ shop/views.py
@@ -45,4 +45,72 @@ class IndexView(View):
 class ShopView(View):
 
     def get(self, request):
-        return render(request, 'shop/shop.html')
+        products_list = [
+            {
+                'name': 'Bell Pepper',
+                'image': 'shop/images/product-1.jpg',
+                'price': '$120.00',
+                'discount': '30%',
+                'price_sale': '$80.00'
+            },
+            {
+                'name': 'Strawberry',
+                'image': 'shop/images/product-2.jpg',
+                'price': '$120.00'
+            },
+            {
+                'name': 'Green Beans',
+                'image': 'shop/images/product-3.jpg',
+                'price': '$120.00'
+            },
+            {
+                'name': 'Purple Cabbage',
+                'image': 'shop/images/product-4.jpg',
+                'price': '$120.00'
+            },
+            {
+                'name': 'Tomatoe',
+                'image': 'shop/images/product-5.jpg',
+                'price': '$120.00',
+                'discount': '30%',
+                'price_sale': '$80.00'
+            },
+            {
+                'name': 'Brocolli',
+                'image': 'shop/images/product-6.jpg',
+                'price': '$120.00'
+            },
+            {
+                'name': 'Carrots',
+                'image': 'shop/images/product-7.jpg',
+                'price': '$120.00'
+            },
+            {
+                'name': 'Fruit Juice',
+                'image': 'shop/images/product-8.jpg',
+                'price': '$120.00'
+            },
+            {
+                'name': 'Onion',
+                'image': 'shop/images/product-9.jpg',
+                'price': '$120.00',
+                'discount': '30%',
+                'price_sale': '$80.00'
+            },
+            {
+                'name': 'Apple',
+                'image': 'shop/images/product-10.jpg',
+                'price': '$120.00'
+            },
+            {
+                'name': 'Garlic',
+                'image': 'shop/images/product-11.jpg',
+                'price': '$120.00'
+            },
+            {
+                'name': 'Chilli',
+                'image': 'shop/images/product-12.jpg',
+                'price': '$120.00'
+            }
+        ]
+        return render(request, 'shop/shop.html', {'product_list': products_list})
