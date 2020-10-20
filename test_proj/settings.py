fd9594b4c856cce83d6d32eb49089bf43e1d2ccb
aeksei <aeksei94@gmail.com>
2020-10-20 16:28:39 +0300

    add translation

--- test_proj/settings.py
+++ test_proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,7 +106,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+LANGUAGES = (
+    ('ru', 'Russian'),
+    ('en', 'English'),
+)
+
+LOCATE_PATHS = (
+    os.path.join(BASE_DIR, 'shop/locale'),
+)
 
 TIME_ZONE = 'UTC'
 
